adcf5f2a70bfb060ab1f72cbf399b5efcd40f151
Frederik Richter <frederik.richter@mailbox.org>
2024-09-09 16:46:40 +0200

    moving kanshi to hm

--- hosts/nixos-thinkpad/configuration.nix
+++ hosts/nixos-thinkpad/configuration.nix
@@ -26,15 +26,6 @@ services.displayManager = {
 
 programs.light.enable = true;
 
-# kanshi systemd service
-  systemd.user.services.kanshi = {
-    description = "kanshi daemon";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
-    };
-  };
-
 # bluetooth
 
 services.blueman.enable = true;
