f8ff44e904c8c5060671b7f0c69f108fbe04c983
Frederik Richter <frederik.richter@mailbox.org>
2024-09-11 18:23:44 +0200

    disable bell

--- hosts/nixos-thinkpad/configuration.nix
+++ hosts/nixos-thinkpad/configuration.nix
@@ -154,6 +154,9 @@ services.pipewire = {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+    
+# disable bell
+  boot.blacklistedKernelModules = [ "pcspkr" ];
 
 # hardware settings
     hardware.opengl = {
