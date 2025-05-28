c2420053bd1396963832b05a41cd86996ca7f38e
Frederik Richter <frederik.richter@mailbox.org>
2025-05-28 20:48:20 +0200

    flake: move top level imports to modules

--- flake.nix
+++ flake.nix
@@ -1,19 +1,25 @@
 {
-  description = "Your NixOS system configuration";
+  description = "NixOS configuration by FrederikRichter";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    nixvim = {
+        url = "github:FrederikRichter/nixvim";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
+  outputs = { nixpkgs, nixvim, nixos-hardware, ... }: {
     nixosConfigurations."nixos-thinkpad" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =  [
-        ./hosts/nixos-thinkpad/configuration.nix
-        ./hosts/nixos-thinkpad/hardware-configuration.nix
-         nixos-hardware.nixosModules.lenovo-thinkpad-l14-intel
+            ./hosts/nixos-thinkpad/configuration.nix
       ];
+      specialArgs = {
+          inherit nixvim;
+          inherit nixos-hardware;
+      };
     };
   };
 }
