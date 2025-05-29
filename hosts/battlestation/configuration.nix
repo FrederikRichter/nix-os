b7bd2471ac374fbd731ec405e199c42a516bb714
Frederik Richter <frederik.richter@mailbox.org>
2025-05-29 15:18:40 +0200

    add: nvidia config

--- hosts/battlestation/configuration.nix
+++ hosts/battlestation/configuration.nix
@@ -1,4 +1,4 @@
-{ host, nixos-hardware, pkgs, lib, ... }:
+{ config, host, nixos-hardware, pkgs, lib, ... }:
 {
   imports = [
     ../default/base.nix
@@ -9,15 +9,31 @@
   services.blueman.enable = lib.mkOverride 101 true;
   hardware.bluetooth.enable = lib.mkOverride 101 true;
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      
-    ];
-  };
 
+    nixpkgs.config.nvidia.acceptLicense = true;
+
+    hardware = {
+        nvidia = {
+            open = false;  # see the note above
+            # Modesetting is required.
+            modesetting.enable = true;
+            nvidiaSettings = true;
+            package = config.boot.kernelPackages.nvidiaPackages.beta;
+        };
+        graphics = {
+            enable = true;
+            extraPackages = with pkgs; [
+                vulkan-loader
+                vulkan-validation-layers
+                vulkan-tools
+            ];
+        };
+    };
  
   environment.sessionVariables = {
+      GBM_BACKEND = "nvidia-drm";
+      LIBVA_DRIVER_NAME = "nvidia";
+      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
   };
 
   system.stateVersion = "25.05";
