c48aa08fffc6f81098de64f242de39d8bde872a8
Frederik Richter <frederik.richter@mailbox.org>
2026-05-19 17:45:02 +0200

    base: fix dns lookups

--- hosts/default/base.nix
+++ hosts/default/base.nix
@@ -1,141 +1,142 @@
 { pkgs, nixvim, lib, host, ... }:
-
 {
-  # Services
-  services.udisks2.enable = true;
-  services.gvfs.enable = true;
-  services.fwupd.enable = true;
+    # Services
+    services.udisks2.enable = true;
+    services.gvfs.enable = true;
+    services.fwupd.enable = true;
 
 
-  boot = {
-      supportedFilesystems = [ "ntfs" ];
-      kernelPackages = pkgs.linuxPackages_latest;
-  };
+    boot = {
+        supportedFilesystems = [ "ntfs" ];
+        kernelPackages = pkgs.linuxPackages_latest;
+    };
 
 
-  services.keyd = {
-      enable = true;
-      keyboards = {
-    # The name is just the name of the configuration file, it does not really matter
-    default = {
-        extraConfig = ''
+    services.keyd = {
+        enable = true;
+        keyboards = {
+            # The name is just the name of the configuration file, it does not really matter
+            default = {
+                extraConfig = ''
             [ids]
             *
 
             [main]
             capslock = esc
             esc = capslock
-      '';
+                '';
+            };
+        };
     };
-  };
-  };
+
+    # Networking
+    networking = {
+        hostName = lib.mkDefault host;
+        useDHCP = false;
+        dhcpcd.enable = false;
+        networkmanager = {
+            enable = true;
+            wifi.powersave = true;
+            dns = "systemd-resolved";
+        };
+        nameservers = [
+            "185.222.222.222"
+            "45.11.45.11"
+        ];
+    };
+
+    services.resolved = {
+        enable = true;
+        settings.Resolve = {
+            DNSSEC = "true";
+            Domains = [ "~." ];
+            DNSOverTLS = "true";
+        };
+    };
+
+    # Environment packages
+    environment.systemPackages = with pkgs; [
+        git
+        home-manager
+    ] ++ [
+            nixvim
+        ];
 
 
-  # Networking
-  networking = {
-      hostName = lib.mkDefault host;
-      useDHCP = false;
-      dhcpcd.enable = false;
-      networkmanager = {
-          enable = true;
-          wifi.powersave = true;
-      };
-      nameservers = [
-          "185.222.222.222"
-          "45.11.45.11"
-      ];
-  };
+    # Nix features
+    nix.settings = {
+        experimental-features = [ "nix-command" "flakes" ];
+        trusted-users = [ "root" "@wheel" ];
+    };
 
-services.resolved = {
-  enable = true;
-  dnssec = "true";
-  domains = [ "~." ];
-  dnsovertls = "true";
-};
+    nix.gc = {
+        automatic = true;
+        dates = "weekly";
+        options = "--delete-older-than 5d";
+    };
 
-  # Environment packages
-  environment.systemPackages = with pkgs; [
-    git
-    home-manager
-  ] ++ [
-  nixvim
-  ];
+    # Time and Locale
+    time.timeZone = "Europe/Berlin";
 
+    i18n.defaultLocale = "en_US.UTF-8";
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "de_DE.UTF-8";
+        LC_IDENTIFICATION = "de_DE.UTF-8";
+        LC_MEASUREMENT = "de_DE.UTF-8";
+        LC_MONETARY = "de_DE.UTF-8";
+        LC_NAME = "de_DE.UTF-8";
+        LC_NUMERIC = "de_DE.UTF-8";
+        LC_PAPER = "de_DE.UTF-8";
+        LC_TELEPHONE = "de_DE.UTF-8";
+        LC_TIME = "de_DE.UTF-8";
+    };
 
-  # Nix features
-  nix.settings = {
-    experimental-features = [ "nix-command" "flakes" ];
-    trusted-users = [ "root" "@wheel" ];
-  };
+    # Users
+    users.users.frederik = {
+        isNormalUser = lib.mkDefault true;
+        description = lib.mkDefault "Frederik Richter";
+        extraGroups = lib.mkDefault [ "networkmanager" "wheel" "video" "podman" ];
+    };
 
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 5d";
-  };
+    programs.nix-ld.enable = true;
 
-  # Time and Locale
-  time.timeZone = "Europe/Berlin";
+    programs.nix-ld.libraries = with pkgs; [
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "de_DE.UTF-8";
-    LC_IDENTIFICATION = "de_DE.UTF-8";
-    LC_MEASUREMENT = "de_DE.UTF-8";
-    LC_MONETARY = "de_DE.UTF-8";
-    LC_NAME = "de_DE.UTF-8";
-    LC_NUMERIC = "de_DE.UTF-8";
-    LC_PAPER = "de_DE.UTF-8";
-    LC_TELEPHONE = "de_DE.UTF-8";
-    LC_TIME = "de_DE.UTF-8";
-  };
+        # Add any missing dynamic libraries for unpackaged programs
+        libc
 
-  # Users
-  users.users.frederik = {
-    isNormalUser = lib.mkDefault true;
-    description = lib.mkDefault "Frederik Richter";
-    extraGroups = lib.mkDefault [ "networkmanager" "wheel" "video" "podman" ];
-  };
-
- programs.nix-ld.enable = true;
-
-  programs.nix-ld.libraries = with pkgs; [
-
-    # Add any missing dynamic libraries for unpackaged programs
-    libc
-    
-  ];
-
-# sudo
-
-security.sudo = {
-  enable = true;
-  extraRules = [{
-    commands = [
-      {
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = "${pkgs.systemd}/bin/reboot";
-        options = [ "NOPASSWD" ];
-      }
-      {
-        command = "${pkgs.systemd}/bin/poweroff";
-        options = [ "NOPASSWD" ];
-      }
     ];
-    groups = [ "wheel" ];
-  }];
-  extraConfig = with pkgs; ''
-      Defaults:picloud secure_path="${lib.makeBinPath [
-      systemd
-      ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
-      '';
-};
-    
 
-# ssh
+    # sudo
+
+    security.sudo = {
+        enable = true;
+        extraRules = [{
+            commands = [
+                {
+                    command = "${pkgs.systemd}/bin/systemctl suspend";
+                    options = [ "NOPASSWD" ];
+                }
+                {
+                    command = "${pkgs.systemd}/bin/reboot";
+                    options = [ "NOPASSWD" ];
+                }
+                {
+                    command = "${pkgs.systemd}/bin/poweroff";
+                    options = [ "NOPASSWD" ];
+                }
+            ];
+            groups = [ "wheel" ];
+        }];
+        extraConfig = with pkgs; ''
+      Defaults:picloud secure_path="${lib.makeBinPath [
+                systemd
+            ]}:/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin"
+        '';
+    };
+
+
+    # ssh
     services.fail2ban.enable = true;
 
     services.openssh = {
@@ -147,14 +148,14 @@ security.sudo = {
         };
     };
 
-environment.sessionVariables = { DOCKER_HOST="unix:///run/user/$UID/podman/podman.sock"; };
+    environment.sessionVariables = { DOCKER_HOST="unix:///run/user/$UID/podman/podman.sock"; };
 
 
-# fonts
+    # fonts
 
-fonts.packages = with pkgs; [ inter libertinus];
+    fonts.packages = with pkgs; [ inter libertinus];
 
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
 }
