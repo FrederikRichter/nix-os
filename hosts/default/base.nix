c7868fbe06696ea6c614dceaea5fbf2a74c9f3bc
Frederik Richter <frederik.richter@mailbox.org>
2026-05-19 18:11:27 +0200

    base: fix networkmanager auto dns

--- hosts/default/base.nix
+++ hosts/default/base.nix
@@ -32,25 +32,34 @@
     # Networking
     networking = {
         hostName = lib.mkDefault host;
+
         useDHCP = false;
         dhcpcd.enable = false;
+
         networkmanager = {
             enable = true;
             wifi.powersave = true;
             dns = "systemd-resolved";
+            settings.main = {ignore-auto-dns = true;};
         };
-        nameservers = [
-            "185.222.222.222"
-            "45.11.45.11"
-        ];
     };
 
     services.resolved = {
         enable = true;
-        settings.Resolve = {
-            DNSSEC = "true";
-            Domains = [ "~." ];
-            DNSOverTLS = "true";
+
+        settings = {
+            Resolve = {
+                DNS = [
+                    "185.222.222.222"
+                    "45.11.45.11"
+                ];
+                FallbackDNS = [
+                    "185.222.222.222"
+                ];
+                DNSSEC = true;
+                DNSOverTLS = true;
+                DNSDefaultRoute = true;
+            };
         };
     };
 
